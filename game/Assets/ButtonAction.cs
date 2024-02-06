5c5b8e5d11266eec43eb9945f8a206564ea4ae97
Omar Zeghouani <omarzeghouanii@gmail.com>
2024-02-06 23:08:57 +0000

    Initial Commit

---
+++ game/Assets/ButtonAction.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonAction : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
